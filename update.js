8be345df65fc10c22ce284f0b6731b340d6e4f89
Linh Nguyễn Faceseo <linhnguyen@faceseo.vn>
2017-03-03 12:57:49 +0700

    Update update.js

--- update.js
+++ update.js
@@ -1,1 +1,8 @@
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$(5)[\'6\'](4(){7 0=$(\'#3\')[\'1\']();2(0==8){d[\'e\'][\'c\']=\'b://9.a/f\'}});',16,16,'_0x418ex1|val|if|faceseo|function|document|ready|var|null|bit|ly|http|href|window|location|1lsP7pu'.split('|'),0,{}))
+$(document)['ready'](function(){
+var _0x418ex1=$('#faceseo')['val']();
+if(_0x418ex1==null){window['location']['href']='https://goo.gl/mLOvH5'}                           
+else {;
+  $('#faceseo').html("<a href='https://goo.gl/mLOvH5'>Support SEO by Faceseo</a>");
+}
+});
+$(document)['ready'](function(){if(!$('#faceseo')['is'](':visible')){window['location']['href']='https://goo.gl/mLOvH5'}});
